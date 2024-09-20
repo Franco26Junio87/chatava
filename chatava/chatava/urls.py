57cf6dd2b7ae8a67c320792b073c78176242fe1b
Franco Avalos <avalosfranco@hotmail.com>
2024-09-19 23:12:22 -0300

    Commit 03: app rooms is updated

--- chatava/chatava/urls.py
+++ chatava/chatava/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
 from rooms import views as rooms_views
 
 urlpatterns = [
     path('', rooms_views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(
+        url='/static/images/favicon.ico')),
 ]
