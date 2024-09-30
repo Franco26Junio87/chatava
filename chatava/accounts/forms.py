8e5c8f3d6109960fec904fea6c05af0585c14e82
Franco Avalos <avalosfranco@hotmail.com>
2024-09-30 15:08:31 -0300

    Commit 09: Improving the Sign Up template.

--- chatava/accounts/forms.py
+++ chatava/accounts/forms.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
 	email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
-	name = forms.CharField(max_length=64, required=False, widget=forms.TextInput())
+	first_name = forms.CharField(max_length=64, required=False, widget=forms.TextInput())
 	#name = forms.CharField(max_length=64, required=False, widget=forms.TextInput(attrs={"placeholder": "Optional"}))
-	surname = forms.CharField(max_length=64, required=False, widget=forms.TextInput())
+	last_name = forms.CharField(max_length=64, required=False, widget=forms.TextInput())
 	#username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Required*"}))
 	class Meta:
 			model = User
-			fields = ('username', 'name', 'surname', 'email', 'password1', 'password2')
+			fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
