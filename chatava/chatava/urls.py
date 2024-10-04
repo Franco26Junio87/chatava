c7af81eb03e9e65a40377c4e0225c665468d94a6
Franco Avalos <avalosfranco@hotmail.com>
2024-10-04 15:34:21 -0300

    Commit 11: Login template is added.

--- chatava/chatava/urls.py
+++ chatava/chatava/urls.py
@@ -24,6 +24,7 @@ from accounts import views as accounts_views
 urlpatterns = [
     path('', rooms_views.home, name='home'),
     path('signup/', accounts_views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
     path('favicon.ico', RedirectView.as_view(
