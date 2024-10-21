204b81e6443053dcdba4c2618af0fa0efd076601
Franco Avalos <avalosfranco@hotmail.com>
2024-10-21 14:46:27 -0300

    Commit 16: Password reset is done.

--- chatava/chatava/urls.py
+++ chatava/chatava/urls.py
@@ -30,29 +30,29 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('reset/', auth_views.PasswordResetView.as_view(
-        template_name='password_reset.html',
-        email_template_name='password_reset_email.html',
-        subject_template_name='password_reset_subject.txt'),
+        template_name='password/password_reset.html',
+        email_template_name='password/password_reset_email.html',
+        subject_template_name='password/password_reset_subject.txt'),
         name='password_reset'),
 
     path('reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='password_reset_done.html'), name='password_reset_done'),
+        template_name='password/password_reset_done.html'), name='password_reset_done'),
 
     path('reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(
-             template_name='password_reset_confirm.html'),
+             template_name='password/password_reset_confirm.html'),
          name='password_reset_confirm'),
 
     path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='password_reset_complete.html'),
+        template_name='password/password_reset_complete.html'),
         name='password_reset_complete'),
     
     path('settings/password/', auth_views.PasswordChangeView.as_view(
-        template_name='password_change.html'),
+        template_name='password/password_change.html'),
         name='password_change'),
         
     path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(
-        template_name='password_change_done.html'),
+        template_name='password/password_change_done.html'),
         name='password_change_done'),
 
     path('admin/', admin.site.urls),
